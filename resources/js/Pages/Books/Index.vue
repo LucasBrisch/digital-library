e957497a1a97bec738fed0d75ae93a61f6aba018
lucasbrisch <lucasbrisch@gmail.com>
2025-11-26 16:47:26 -0300

    first view, routes and controllers

---
+++ resources/js/Pages/Books/Index.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <h1>Lista de Livros</h1>
+
+    <ul>
+        <li v-for="book in books" :key="book.id">
+            {{ book.title }} — {{ book.writer_name }}
+        </li>
+    </ul>
+</template>
+
+<script setup>
+defineProps({
+    books: Array
+});
+</script>
